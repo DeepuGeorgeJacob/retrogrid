1e87af2bbc7e775a921b1cc3282ea155f2d2a673
Deepu George Jacob <deepugeorgejacob@Deepus-MacBook-Pro.local>
2023-03-19 10:07:44 +0100

    Property variable added

--- app/src/main/java/com/app/retrogrid/api/RetrogridService.kt
+++ app/src/main/java/com/app/retrogrid/api/RetrogridService.kt
@@ -1,8 +1,6 @@
 package com.app.retrogrid.api
 
-import com.app.retrogrid.annotation.ErrorResponseMap
 import com.app.retrogrid.annotation.RetrofitServiceConfiguration
-import com.app.retrogrid.model.NewsErrorResponse
 import com.app.retrogrid.model.NewsOverview
 import com.app.retrogrid.response.RetrogridResponse
 import retrofit2.http.GET
@@ -12,3 +10,4 @@ interface RetrogridService {
     @GET(value = "everything?domains=wsj.com&apiKey=a93a86ca5d144c2dbd676e53fb096c680")
     suspend fun fetchNewsOverview():RetrogridResponse<NewsOverview>
 }
+
