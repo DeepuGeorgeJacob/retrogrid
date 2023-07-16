6bbe53a0e7e75ffdbd75ba214d6f8681e55e6bf3
Deepu George Jacob <deepugeorgejacob@Deepus-MacBook-Pro.local>
2023-07-16 12:45:12 +0200

    Updared with eqal and hashcode in ServiceLayerConfiguration

--- retrogriod-network/src/main/java/com/app/retrogrid/configuration/ServiceLayerConfiguration.kt
+++ retrogriod-network/src/main/java/com/app/retrogrid/configuration/ServiceLayerConfiguration.kt
@@ -8,4 +8,29 @@ data class ServiceLayerConfiguration(
     val errorResponseClass: KClass<*>? = null,
     val interceptors: Array<KClass<out Interceptor>>? = emptyArray(),
     val isDefaultNetworkLogEnabled: Boolean = false
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ServiceLayerConfiguration
+
+        if (baseUrl != other.baseUrl) return false
+        if (errorResponseClass != other.errorResponseClass) return false
+        if (interceptors != null) {
+            if (other.interceptors == null) return false
+            if (!interceptors.contentEquals(other.interceptors)) return false
+        } else if (other.interceptors != null) return false
+        if (isDefaultNetworkLogEnabled != other.isDefaultNetworkLogEnabled) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = baseUrl?.hashCode() ?: 0
+        result = 31 * result + (errorResponseClass?.hashCode() ?: 0)
+        result = 31 * result + (interceptors?.contentHashCode() ?: 0)
+        result = 31 * result + isDefaultNetworkLogEnabled.hashCode()
+        return result
+    }
+}
